c94c245e0289198309e8242c6327fed4ac18b7b3
Adit841 <adityagour0841@gmail.com>
2026-07-14 19:21:36 +0530

    adding  middleware

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -4,18 +4,6 @@ const { uploadFile } = require("../services/storage.service");
 const albumModel = require("../models/album.model");
 
 async function createMusic(req, res) {
-  console.log("Cookies received:", req.cookies);
-  const token = req.cookies.token;
-
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorise" });
-  }
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (decoded.role !== "artist") {
-      return res.status(403).json({ message: "You dont have access" });
-    }
-
     const { title } = req.body;
     const file = req.file;
 
@@ -24,7 +12,7 @@ async function createMusic(req, res) {
     const music = await musicModel.create({
       uri: result.url,
       title,
-      artist: decoded.id,
+      artist: req.user.id,
     });
 
     res.status(201).json({
@@ -36,34 +24,15 @@ async function createMusic(req, res) {
         artist: music.artist,
       },
     });
-  } catch (err) {
-    console.log("Error in createMusic:", err.message);
-    return res.status(401).json({
-      message: "Unauthorised",
-    });
-  }
+  
 }
 
 async function createAlbum(req, res) {
-     console.log("req.body:", req.body);
-  console.log("typeof musics:", typeof req.body.musics, Array.isArray(req.body.musics));
-  
-  const token = req.cookies.token;
-
-  if (!token) {
-    return res.status(401).json({ message: "unauthorised" });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (decoded.role !== "artist") {
-      return res.status(403).json({ message: "You dont have access" });
-    }
 
     const {title, musics} = req.body;
     const album = await albumModel.create({
         title,
-        artist: decoded.id,
+        artist: req.user.id,
         music: musics
     })
 
@@ -75,10 +44,6 @@ async function createAlbum(req, res) {
             music: album.music
         }
     })
-  } catch (err) {
-    console.log(err);
-    return res.status(401).json({ message: "Unauthorised" });
-  }
 }
 
 module.exports = { createMusic, createAlbum };
